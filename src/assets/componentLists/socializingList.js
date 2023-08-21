5e0be884c75909dce9220841514f59d778d4ae4f
O. Sharpe <osharpe001@gmail.com>
2023-08-20 22:09:16 -0400

    set icons and urls in socializingList.js

---
+++ src/assets/componentLists/socializingList.js
@@ -0,0 +1,29 @@
+import { email } from "../images/socialIcons/email.png";
+import { github } from "../images/socialIcons/github.png";
+import { linkedin } from "../images/socialIcons/linkedin.png";
+import { stackoverflow } from "../images/socialIcons/stackoverflow.png";
+import { twitter } from "../images/socialIcons/twitter.png";
+
+
+export const socials = [
+    {
+      icon: email,
+      url: "mailto: osharpe001@gmail.com",
+    },
+    {
+      icon: github,
+      url: "https://github.com/OSharpe001",
+    },
+    {
+      icon: linkedin,
+      url: "https://www.linkedin.com/in/omarisharpe",
+    },
+    {
+      icon: twitter,
+      url: "https://twitter.com/OSharpe001",
+    },
+    {
+      icon: stackoverflow,
+      url: "https://stackoverflow.com/users/19149328/o-sharpe",
+    },
+  ];
