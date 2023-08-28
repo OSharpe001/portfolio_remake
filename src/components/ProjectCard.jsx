9c8992a3b146f36a9356dd53c82a43e2ff34f334
O. Sharpe <osharpe001@gmail.com>
2023-08-27 23:21:53 -0400

    started on ProjectCard and added arrow image

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-export default function ProjectCard() {
+export default function ProjectCard({ title, description, image, site }) {
   return (
-    <div>
-        <h1>ProjectCard</h1>
+    <div className="project-card">
+        <img src={image} alt={`${title} website`} />
+        <h2>{title}</h2>
+        <p>{description}</p>
+        {
+          site ?
+            <button>
+              <a
+                aria-label="On Click"
+                target="_blank"
+                rel="noreferrer"
+                href={site}
+              >
+                Visit Site <img src="../assets/images/icons/double_right_arrow.png" alt="right arrow" />
+              </a>
+            </button>
+          :
+            null
+        }
+
     </div>
   );
 };
