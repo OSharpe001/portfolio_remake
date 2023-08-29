774aab1ef051b0f40bdc308fb056d507ff42fae2
O. Sharpe <osharpe001@gmail.com>
2023-08-28 23:58:51 -0400

    added info to projectsList

--- src/assets/componentLists/projectList.js
+++ src/assets/componentLists/projectList.js
@@ -2,43 +2,55 @@ export const projects = [
     {
       title: "Letter to Success",
       description:
-        'My rendition of "Wheel of Fortune! A great way to wind-down after a busy day of coding. 😁 ',
-      getImageSrc: () => require("../images/projectPics/letter_to_success.png"),
-      site: "https://lettertosuccess.netlify.app"
+        'My rendition of "Wheel of Fortune! A great way to wind-down after a busy day of coding.',
+      image: require("../images/projectPics/letter_to_success.png"),
+      site: "https://lettertosuccess.netlify.app",
+      githubLink: "https://github.com/OSharpe001/letter-to-success",
+      position: "center"
     },
     // {
     //   title: "Little Lemon",
     //   description:
     //     "My rendition of a bistro's website, complete with online ordering for delivery.",
-    //   getImageSrc: () => require("../images/little_lemon1.png"),
-    //   site: "https://littlelemonbistro.vercel.app"
+    //   image: require("../images/little_lemon1.png"),
+    //   site: "https://littlelemonbistro.vercel.app",
+    // githubLink: "https://github.com/OSharpe001/little-lemon",
+    // position: "right"
     // },
     {
       title: "Our Tip Calculator",
       description:
         "An intuitive tip calculator for any occasion.",
-      getImageSrc: () => require("../images/projectPics/our_tip_calculator.png"),
-      site: "https://ourtipcalculator.netlify.app"
+      image: require("../images/projectPics/our_tip_calculator.png"),
+      site: "https://ourtipcalculator.netlify.app",
+      githubLink: "https://github.com/OSharpe001/myTipCalculator",
+      position: "right"
     },
     {
       title: "Pokemon Memory Match Game",
       description:
         `A tournament-style, card memory-match game that's fun for all ages!`,
-      getImageSrc: () => require("../images/projectPics/poke_memory_match.png"),
-      site: "https://osharpe001.github.io"
+      image: require("../images/projectPics/poke_memory_match.png"),
+      site: "https://osharpe001.github.io",
+      githubLink: "https://github.com/OSharpe001/OSharpe001.github.io",
+      position: "right"
     },
     // {
     //   title: "The Sharpe Pages",
     //   description:
     //     'My first full-stack "CRUD-app" (2021)! Although it may need visual polishing, it is fully functional.',
-    //   getImageSrc: () => require("../images/sharpe_pages.png"),
-    //   site: "https://osharpe.pythonanywhere.com"
+    //   image: require("../images/sharpe_pages.png"),
+    //   site: "https://osharpe.pythonanywhere.com",
+    // githubLink: null,
+    // position: "right"
     // },
     {
       title: "More to Come",
       description:
         "Just like a city's skyline, there's more projects on the horizon. . .",
-      getImageSrc: () => require("../images/projectPics/build_higher.jpeg"),
-      site: null
+      image: require("../images/projectPics/build_higher.jpeg"),
+      site: null,
+      githubLink: null,
+      position: "right"
     },
   ];
