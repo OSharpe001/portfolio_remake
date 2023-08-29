5c521468562f13d135cc08c92992042de10ab37a
O. Sharpe <osharpe001@gmail.com>
2023-08-29 08:51:32 -0400

    added resume pdf and link in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -62,6 +62,7 @@ export default function Header() {
         <nav className="section-scroll">
             <a aria-label="On Click" className="shift-page-link" onClick={handlePageShift("contactme")} href="/#contact-me">Contact Me</a>
             <a aria-label="On Click" className="shift-page-link" onClick={handlePageShift("projects")} href="/#projects">Projects</a>
+            <a aria-label="On Click" className="shift-page-link"  target="_blank" rel="noreferrer" href="Omari_Sharpe_SE_Resume.pdf">My Resume</a>
         </nav>
     </header>
   );
