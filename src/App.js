f32da0218978240e07774c0aec7f418e19251918
O. Sharpe <osharpe001@gmail.com>
2023-08-28 23:59:27 -0400

    discarded useNavigate from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AboutMe, Footer, Header, Main, ProjectDetailPage } from './components';
 
 
 export default function App() {
-
-  const navigate = useNavigate();
 
   return (
     <div className="App">
