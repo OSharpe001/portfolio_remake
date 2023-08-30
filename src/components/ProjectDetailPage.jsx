f97cf89a70cec0af531c5a09378540de2f5877b8
O. Sharpe <osharpe001@gmail.com>
2023-08-29 22:30:57 -0400

    fixed project detail page!

--- src/components/ProjectDetailPage.jsx
+++ src/components/ProjectDetailPage.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
-export default function ProjectDetailPage({ title, details, image}) {
+
+export default function ProjectDetailPage({ form }) {
+
+  const title = form.title;
+  const details = form.details;
+  const image = form.image;
+  const site = form.site;
+  const github = form.github;
+  const techUsed = form.techUsed;
+
+  console.log("PROJECTDETAILSPAGE FORM INFO: ", form);
+  // console.log("PROJECTDETAILSPAGE GITHUBLINK INFO: ", github);
+
   return (
     <section className="details-page">
-        <h1>ProjectDetailPage</h1>
-        <img src={image} alt={`${title} web app`} />
-        <h2>{title}</h2>
-        <p>{details}</p>
-
-
+        <h1 className='details-h1' >ProjectDetailPage</h1>
+        <img className='details-image'  src={image} alt={`${title} web app`} />
+        <h2 className='details-title' >{title}</h2>
+        <p className='details' >{details}</p>
+        <p className="details-tech-used" >{techUsed}</p>
+        <button className='details-site-button' ><a className='details-site-link'  target="_blank" rel="noreferrer" href={site}>{title}</a></button>
+        <button className='details-github-button' ><a className='details-github-link'  target="_blank" rel="noreferrer" href={github}>{`${title}'s GitHub Repo`}</a></button>
+        <button className="details-site-button"><a href="/">Go Back</a></button>
     </section>
   );
 };
