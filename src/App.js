fd8af313e23082090120e86fd33cdb485c3cda55
O. Sharpe <osharpe001@gmail.com>
2023-08-29 22:34:24 -0400

    added form useState to send info to details page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from "react";
 import './App.css';
 import { AboutMe, Footer, Header, Main, ProjectDetailPage } from './components';
 
 
 export default function App() {
 
+  const [form, setForm] = useState({
+    title: "",
+    details: "",
+    image: "",
+    site: "",
+    github: "",
+  });
+
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Main />}/>
-        <Route path="/details" element={<ProjectDetailPage />}/>
+        <Route path="/" element={<Main
+                                    setForm={setForm}
+                                  />}/>
+        <Route path="/details" element={<ProjectDetailPage
+                                                        form={form}
+                                                      />}/>
         <Route path="/aboutme" element={<AboutMe />}/>
       </Routes>
       <Footer />
