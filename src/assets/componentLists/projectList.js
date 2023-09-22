e4ea1d24dec12f0fc8729f668c33823afa928195
O. Sharpe <osharpe001@gmail.com>
2023-09-22 17:53:22 -0400

    commented out useless projects section items

--- src/assets/componentLists/projectList.js
+++ src/assets/componentLists/projectList.js
@@ -21,71 +21,71 @@ export const projects = [
     githubLink: "https://github.com/OSharpe001/anime_hunter",
     position: "right"
   },
-    {
-      title: "Letter to Success",
-      description:
-        'My rendition of "Wheel of Fortune! A great way to wind-down after a busy day of coding.',
-      details: `My first vanity project after learning React! This took me a month and a week to
-      complete (2/2023 - 4/2023)!`,
-      techUsed: "TECHNOLOGY USED: JavaScript, HTML, CSS, React",
-      image: require("../images/projectPics/letter_to_success.png"),
-      site: "https://lettertosuccess.netlify.app",
-      githubLink: "https://github.com/OSharpe001/letter-to-success",
-      position: "right"
-    },
-    // {
-    //   title: "Little Lemon",
-    //   description:
-    //     "My rendition of a bistro's website, complete with online ordering for delivery.",
-    //   details: ``,
-    //   techUsed: "TECHNOLOGY USED: ",
-    //   image: require("../images/little_lemon1.png"),
-    //   site: "https://littlelemonbistro.vercel.app",
-    //   githubLink: "https://github.com/OSharpe001/little-lemon",
-    //   position: "right"
-    // },
-    {
-      title: "Our Tip Calculator",
-      description:
-        "An intuitive tip calculator for any occasion.",
-        details: `NOT FILLED IN, AS YET...`,
-        techUsed: "TECHNOLOGY USED: ",
-      image: require("../images/projectPics/our_tip_calculator.png"),
-      site: "https://ourtipcalculator.netlify.app",
-      githubLink: "https://github.com/OSharpe001/myTipCalculator",
-      position: "right"
-    },
-    {
-      title: "Pokemon Memory Match Game",
-      description:
-        `A tournament-style, card memory-match game that's fun for all ages!`,
-        details: `NOT FILLED IN, AS YET...`,
-        techUsed: "TECHNOLOGY USED: ",
-      image: require("../images/projectPics/poke_memory_match.png"),
-      site: "https://osharpe001.github.io",
-      githubLink: "https://github.com/OSharpe001/OSharpe001.github.io",
-      position: "right"
-    },
-    // {
-    //   title: "The Sharpe Pages",
-    //   description:
-    //     'My first full-stack "CRUD-app" (2021)! Although it may need visual polishing, it is fully functional.',
-    //   details: "",
-    //   techUsed: "TECHNOLOGY USED: ",
-    //   image: require("../images/sharpe_pages.png"),
-    //   site: "https://osharpe.pythonanywhere.com",
-    //   githubLink: null,
-    //   position: "right"
-    // },
-    {
-      title: "More to Come",
-      description:
-        "Just like a city's skyline, there's more projects on the horizon. . .",
-      details: `I'll keep on building!`,
-      techUsed: "TECHNOLOGY USED: The latest technologies",
-      image: require("../images/projectPics/build_higher.jpeg"),
-      site: null,
-      githubLink: null,
-      position: "right"
-    },
-  ];
+  {
+    title: "Pokemon Memory Match Game",
+    description:
+      `A tournament-style, card memory-match game that's fun for all ages!`,
+      details: `NOT FILLED IN, AS YET...`,
+      techUsed: "TECHNOLOGY USED: ",
+    image: require("../images/projectPics/poke_memory_match.png"),
+    site: "https://osharpe001.github.io",
+    githubLink: "https://github.com/OSharpe001/OSharpe001.github.io",
+    position: "right"
+  },
+  {
+    title: "Letter to Success",
+    description:
+      'My rendition of "Wheel of Fortune! A great way to wind-down after a busy day of coding.',
+    details: `My first vanity project after learning React! This took me a month and a week to
+    complete (2/2023 - 4/2023)!`,
+    techUsed: "TECHNOLOGY USED: JavaScript, HTML, CSS, React",
+    image: require("../images/projectPics/letter_to_success.png"),
+    site: "https://lettertosuccess.netlify.app",
+    githubLink: "https://github.com/OSharpe001/letter-to-success",
+    position: "right"
+  },
+  // {
+  //   title: "Little Lemon",
+  //   description:
+  //     "My rendition of a bistro's website, complete with online ordering for delivery.",
+  //   details: ``,
+  //   techUsed: "TECHNOLOGY USED: ",
+  //   image: require("../images/little_lemon1.png"),
+  //   site: "https://littlelemonbistro.vercel.app",
+  //   githubLink: "https://github.com/OSharpe001/little-lemon",
+  //   position: "right"
+  // },
+  // {
+  //   title: "Our Tip Calculator",
+  //   description:
+  //     "An intuitive tip calculator for any occasion.",
+  //     details: `NOT FILLED IN, AS YET...`,
+  //     techUsed: "TECHNOLOGY USED: ",
+  //   image: require("../images/projectPics/our_tip_calculator.png"),
+  //   site: "https://ourtipcalculator.netlify.app",
+  //   githubLink: "https://github.com/OSharpe001/myTipCalculator",
+  //   position: "right"
+  // },
+  // {
+  //   title: "The Sharpe Pages",
+  //   description:
+  //     'My first full-stack "CRUD-app" (2021)! Although it may need visual polishing, it is fully functional.',
+  //   details: "",
+  //   techUsed: "TECHNOLOGY USED: ",
+  //   image: require("../images/sharpe_pages.png"),
+  //   site: "https://osharpe.pythonanywhere.com",
+  //   githubLink: null,
+  //   position: "right"
+  // },
+//   {
+//     title: "More to Come",
+//     description:
+//       "Just like a city's skyline, there's more projects on the horizon. . .",
+//     details: `I'll keep on building!`,
+//     techUsed: "TECHNOLOGY USED: The latest technologies",
+//     image: require("../images/projectPics/build_higher.jpeg"),
+//     site: null,
+//     githubLink: null,
+//     position: "right"
+//   },
+];
