9f9943e37917f754b0921f02e0eea8878bc1b3c5
O. Sharpe <osharpe001@gmail.com>
2024-07-24 20:34:56 -0400

    resume landing page change.

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -4,11 +4,18 @@ export default function LandingSection() {
 
   return (
       <section className="landing">
-        <h1 className="console-greeting">{"<h1>Hello, World!</h1>"}</h1>
+        <h1 className="console-greeting">Hello, World!</h1>
         <img src={require("../assets/images/my_profile.png")} alt="my profile" className="profile-pic" />
         <h1 className="introduction">I am Omari Sharpe!</h1>
-        <h1 className="declaration">A Full-Stack Web Developer</h1>
-        <h1 className="declaration">specialized in React</h1>
+        <h1 className="declaration">Your Friendly Neighborhood Full-Stack
+          <br />
+          <br />
+          Web Developer
+          <br />
+          <br />
+          Who Specializes In React
+        </h1>
+        
       </section>
   );
 };
