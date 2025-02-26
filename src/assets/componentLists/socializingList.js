b78ee812279f9e2c29a48fcf24bf90d600d6ff63
O. Sharpe <osharpe001@gmail.com>
2025-02-25 19:07:54 -0500

    slight cleanup

--- src/assets/componentLists/socializingList.js
+++ src/assets/componentLists/socializingList.js
@@ -1,7 +1,6 @@
 import email from "../images/socialIcons/email.png";
 import github from "../images/socialIcons/github.png";
 import linkedin from "../images/socialIcons/linkedin.png";
-// import stackoverflow from "../images/socialIcons/stackoverflow.png";
 import X from "../images/socialIcons/xIcon.png";
 import youtube from "../images/socialIcons/youtube.png";
 
@@ -23,10 +22,6 @@ export const socials = [
       icon: X,
       url: "https://twitter.com/OSharpe001",
     },
-    // {
-    //   icon: stackoverflow,
-    //   url: "https://stackoverflow.com/users/19149328/o-sharpe",
-    // },
     {
       icon: youtube,
       url: "https://www.youtube.com/channel/UCSkr3q6LHUwPqWdCdQZY8ZA",
